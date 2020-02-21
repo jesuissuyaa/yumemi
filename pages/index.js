caa401c78bbba29b06d89ddcd59132aaa7b7baf6
jesuissuyaa <oriko.harada@uec.ac.jp>
2020-02-22 03:39:09 +0900

    APIレスポンス取得; 都道府県一覧を生成

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import instance from "@/utils/instance";
 
 const Index = () => {
-  return <p>page</p>;
+  // 都道府県のデータ
+  const [data, setData] = useState([{ prefCode: 0, prefName: "" }]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await instance.get("/api/v1/prefectures");
+      setData(res.data.result);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h1>都道府県別の人口推移グラフ</h1>
+
+      <section>
+        <h2>都道府県</h2>
+        <div>
+          {data.map(item => (
+            <>
+              <input
+                type="checkbox"
+                name={item.prefName}
+                value={item.prefCode}
+              />
+              <label htmlFor={item.prefName}>{item.prefName}</label>
+            </>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Index;
