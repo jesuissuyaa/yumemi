36dd8311c33f5355d7643977068ab62f0cd93197
jesuissuyaa <oriko.harada@uec.ac.jp>
2020-02-22 12:08:44 +0900

    stateのデータ構造を整理

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,10 @@ const Index = () => {
   const [cmps, setCmps] = useState([]);
   // チェックされた都道府県のprefCode
   const [checked, setChecked] = useState([]);
+  // 年のデータ; 1960-2045まで5年刻み
+  const years = [...Array(18)].map((_, i) => 1960 + i * 5);
+  // 人口構成のデータ
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,12 +19,57 @@ const Index = () => {
       setPrefs(res.data.result);
     };
     fetchData();
+    // 人口構成のstateを初期化
+    setData(years.map(item => ({ year: item })));
   }, []);
+
+  // DEBUG
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   // チェックボックスの状態が変わったときのハンドラー
   // 人口データの取得とチェックされた都道府県リストの更新をする
   const handleChange = async event => {
-    const value = event.target.value;
+    // const value = event.target.value;
+    const prefCode = parseInt(event.target.value, 10);
+    const prefName = prefs.find(item => item.prefCode === prefCode).prefName;
+
+    console.log(prefName);
+    const keys = Object.keys(data[0]); // グラフ用データのキー
+
+    // checked --> uncheckedの場合
+    if (keys.includes(prefName)) {
+      // stateを更新
+      setData(
+        // TODO: もっと良い消し方があれば修正
+        data.map(item => {
+          delete item[prefName];
+          return item;
+        })
+      );
+    }
+    // checked --> uncheckedの場合
+    if (!keys.includes(prefName)) {
+      // APIから人口データを取得
+      const res = await instance.get("/api/v1/population/composition/perYear", {
+        params: {
+          prefCode,
+          cityCode: "-" // 市区町村はすべて
+        }
+      });
+      // responseから総人口のデータだけ格納
+      const population = res.data.result.data[0].data;
+      setData(
+        data.map((item, i) => {
+          // 都道府県:人口 のpropertyを追加
+          const obj = {};
+          obj[prefName] = population[i].value;
+          return { ...item, ...obj };
+        })
+      );
+    }
+    /*
     // データがなければ追加
     if (!cmps.some(item => item.prefCode === value)) {
       // APIから人口データを取得
@@ -43,15 +92,16 @@ const Index = () => {
     if (checked.includes(value)) {
       setChecked(checked.filter(item => item != value));
     }
+    */
   };
 
   // DEBUG
-  useEffect(() => {
-    console.log(checked);
-  }, [checked]);
-  useEffect(() => {
-    console.log(cmps);
-  }, [cmps]);
+  // useEffect(() => {
+  //   console.log(checked);
+  // }, [checked]);
+  // useEffect(() => {
+  //   console.log(cmps);
+  // }, [cmps]);
 
   return (
     <>
